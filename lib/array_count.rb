463fbdaf866b0fb295b6b14a1638c519f162329d
Zoe Xanos <zxanos@gmail.com>
2020-05-10 08:30:17 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -6,3 +6,6 @@ count_strings(array_1)
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
+
+array_2 = ["", 4, "", "goodbye",""]
+count_empty_strings
